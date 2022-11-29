a3ce54edad8c96bd55d89050e4f091a0cddf67d9
BORÁŇ Branislav <branislav.boran@asseco-ce.com>
2022-11-29 18:15:14 +0100

    ADDED: vehicles in Jur into dump and source files

---
+++ sqlTablesData/sensorsInsert.sql
@@ -0,0 +1,5 @@
+INSERT INTO sensors (id_sensor,place,gps) VALUES (5,"Trnavske myto","48.15839365809828,17.128922790334137");
+INSERT INTO sensors (id_sensor,place,gps) VALUES (6,"Trnavske myto","48.15839365809828,17.128922790334137");
+INSERT INTO sensors (id_sensor,place,gps) VALUES (8,"Raca","48.21126991552776,17.15794898683604");
+INSERT INTO sensors (id_sensor,place,gps) VALUES (2,"Raca","48.21126991552776,17.15794898683604");
+INSERT INTO sensors (id_sensor,place,gps) VALUES (11,"Svaty jur","48.2483564997389,17.21807728112588");
